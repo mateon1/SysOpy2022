4268f136b3c9e452365848b8c7b9d1b2ac9979b2
Mateusz Naściszewski <matin1111@wp.pl>
2022-03-04 12:57:41 +0100

    Lab1, z1: Write library header file

---
+++ lab1/zad1/libwc.h
@@ -0,0 +1,46 @@
+// Mateusz Naściszewski, 2022
+
+#pragma once
+
+#include <stdbool.h> // bool
+#include <stdint.h> // (u)intX_t
+
+#ifndef _LIBWC_NO_OPQAUE_TYPES
+typedef void* libwc_context;
+#endif
+
+// --- Context management ---
+
+// Creates an empty libwc context with default settings
+libwc_context libwc_create(void);
+// Creates an empty libwc context with a custom temporary filepath
+// Lifetime note: the path string must not be freed before calling libwc_destroy
+libwc_context libwc_create_tmpfile(char* tmpfile);
+
+// Destroys the libwc context and frees all allocated data.
+// Removes the temporary file, if it exists.
+void libwc_destroy(libwc_context);
+
+// --- Functionality ---
+
+// Performs a `wc` count for the specified files, separated by spaces, and saves the result into a temporary file.
+void libwc_read_to_tmpfile(libwc_context, char* filepaths);
+
+// Loads result from the temporary file.
+// If successful, returns a non-negative integer handle to an internal results table.
+// If unsuccessful, but the libwc context is left in a valid state, returns -1.
+// Otherwise, returns -2 or less, and the context must be destroyed without any further use.
+int32_t libwc_load_result(libwc_context);
+
+// Delete result matching a given handle.
+// Returns true if deleted successfully, false otherwise (e.g. invalid index, already deleted, etc.)
+// Safety: Always safe to call, even with invalid or already freed indexes.
+bool libwc_del_result(libwc_context, int32_t handle);
+
+
+// Yes, the spec does not require providing any functionality for actually reading the managed data.
+// But here it is anyway, mostly for debugging.
+
+// Returns a reference to a managed result.
+// Lifetime note: The resulting pointer is only valid as long as the context remains unmodified, calling libwc_load_result or libwc_del_result (or libwc_destroy) may invalidate the pointer.
+char* libwc_get_result(libwc_context, int32_t handle);
